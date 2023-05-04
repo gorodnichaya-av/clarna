404a3b87ae57b628e1a0f5f18ce633e99c699e14
ishunter216 <michael.king@edenprogram.com>
2023-05-04 10:17:19 +0300

    horizonatl scroll

--- js/script.js
+++ js/script.js
@@ -229,6 +229,42 @@ $(function () {
       }
     });
 
+    // Horizontall Scroll
+
+    let holder = $(".js-holder"),
+        list = holder.find(".js-holder-list"),
+        clonedList = list.clone();
+    var widthArr = [];
+
+    list.find('li').each(function() {
+      widthArr.push($(this).outerWidth(true, true));
+    });
+
+    let listWidth = widthArr.reduce((a, b) => {
+      return a + b;
+    }, 10);
+    
+    var endPos = holder.width() - listWidth;
+    list.add(clonedList).css({
+      "width" : listWidth + "px"
+    });
+    clonedList.addClass("cloned").appendTo(holder);
+    //TimelineMax
+    var infinite = new TimelineMax({repeat: -1, paused: false});
+    var time = 5;
+    infinite.fromTo(list, time, {left:0}, {left: -listWidth, ease: Linear.easeNone}, 0);
+    infinite.fromTo(clonedList, time, {left:listWidth}, {left:0, ease: Linear.easeNone}, 0);
+    infinite.set(list, {left: listWidth});
+    infinite.to(clonedList, time, {left: -listWidth, ease: Linear.easeNone}, time);
+    infinite.to(list, time, {left: 0, ease: Linear.easeNone}, time);
+    //Pause/Play
+    holder.on("mouseenter", function(){
+      infinite.pause();
+    }).on("mouseleave", function(){
+      infinite.play();
+    });
+
+
     // Accordion
     $('.js-accordion').on('click', '.js-accordion-btn', function(){
       let parent = $(this).parent();
